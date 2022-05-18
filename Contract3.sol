45a94fdd0e4c61858cc81b8845f0743ed9387c61
Jian Kim <jian7061@gmail.com>
2022-05-18 09:57:17 +0900

    String manipulations: concat,comparison, reverse

---
+++ Contract3.sol
@@ -0,0 +1,39 @@
+//String manipulations in Solidity: concatenation, string comparison, reverse etc...
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    function length(string calldata str) external pure returns (uint256) {
+        return bytes(str).length;
+    }
+
+    function concatenate(string calldata a, string calldata b)
+        external
+        pure
+        returns (string memory)
+    {
+        return string(abi.encodePacked(a, b));
+    }
+
+    function reverse(string calldata _str)
+        external
+        pure
+        returns (string memory)
+    {
+        bytes memory str = bytes(_str);
+        string memory tmp = new string(str.length);
+        bytes memory _reverse = bytes(tmp);
+
+        for (uint256 i = 0; i < str.length; i++) {
+            _reverse[str.length - i - 1] = str[i];
+        }
+        return string(_reverse);
+    }
+
+    function compare(string calldata a, string calldata b)
+        external
+        pure
+        returns (bool)
+    {
+        return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
+    }
+}
